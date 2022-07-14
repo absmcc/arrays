709f0e09a881f9e84b96e8605957874846f3c153
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-14 11:38:24 -0400

    Into to Arrays

---
+++ array.js
@@ -0,0 +1,24 @@
+let fruits = ["kiwi", "mango", "apple"]
+fruits.push("avocado")
+// console.log(fruits)
+
+
+// console.log(fruits)
+// console.log(...fruits)
+// console.log('This is reg display' + fruits)
+// console.log('This is a spread', ...fruits)
+
+fruits.unshift('star fruit')
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+fruits.sort()
+console.log(fruits)
+fruits.indexOf("mango")
+console.log(fruits.indexOf('Maria'))
+console.log(fruits.indexOf('mango'))
+
+const chickenEgg = ['🐔','🥚'].sort()
+console.log(`The ${chickenEgg[0]} came first`)
